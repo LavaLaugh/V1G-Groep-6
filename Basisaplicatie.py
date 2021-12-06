b6fa50abba2a06497d4e2c204eb512a5c7fc26f5
Luc Conraads <lmaconraads@gmail.com>
2021-12-06 16:19:06 +0100

    2 functies aan Basisaplicatie toegevoegd

--- Basisaplicatie.py
+++ Basisaplicatie.py
@@ -1,9 +1,19 @@
 import json
 
-open_jayson = open('steam.json')
 
-data = json.load(open_jayson)
+def naam_game_1():
+    open_jayson = open('steam.json')
+    data = json.load(open_jayson)
+    game_1 = data[0]
+    open_jayson.close()
+    return game_1.get('name')
 
-for i in data:
-    print(i)
-    print('data')
+
+def sorteren_basis(input):
+    sorted_games = []
+    open_jayson = open('steam.json')
+    data = json.load(open_jayson)
+    sorted_data = sorted(data, key=lambda d: d[input].lower())
+    for game in sorted_data:
+        sorted_games.append(game)
+    return sorted_games
